f5fbbc97e98230a8a5d452074318888f3a8a67cf
Pier Bover <pierbover11@gmail.com>
2021-03-01 11:38:04 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/', {
+	handler (request, reply) {
+		return 'Hello world!';
+	}
+})
+
+const start = async () => {
+	try {
+		await fastify.listen(3000);
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+}
+
+start();
